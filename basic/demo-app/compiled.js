42eb51aec39c63ff51ea1edf50e0bf43390eecae
to3izo <to3izo3@gmail.com>
2024-11-09 22:41:37 +0900

    basic/demo-app/compiled.js をGitの追跡対象から除外

--- basic/demo-app/compiled.js
+++
@@ -1,8 +0,0 @@
-const {
-  createRoot
-} = ReactDOM;
-
-// ブラウザの DOM ノード内に react コンポーネントのルートを作成
-const root = createRoot(document.getElementById('root'));
-// react ノードをレンダリング（＝表示）する
-root.render(/*#__PURE__*/React.createElement("h1", null, "Hello, React!!"));
